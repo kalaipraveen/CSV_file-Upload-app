f8390bd7f33b51798ab3045aafd2e801cb6bc3b6
kalaipraveen <79854541+kalaipraveen@users.noreply.github.com>
2021-05-01 14:52:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Grootan CSV Upload App
+
+Steps followed
+
+1.Created a MongoDB database
+2.Generated an application, to import CSV file using NodeJS
+3.Installed Mongoose module to connect and process data using mongoose application.
+4.Installed Fast-CSV module to import CSV file using nodejs into MongoDB collection
+5.Installed flash-connect module to display a success flash message
+6.Used Ajax GET request to fetch and display data using Nodejs and MongoDB
+
+Note: 
+1. To Run MongoDB and on command line type command: 
+    use dbproducts
+2. To launch Web Grootan CSV Upload application run the below command:
+     npm start
+
+ 
---
+++ routes/index.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var csv = require("fast-csv");
+var router = express.Router();
+var fs = require('fs');
+
+var mongoose = require('mongoose');
+const { finished } = require('stream');
+
+var Product  = mongoose.model('Products');
+
+var csvfile = __dirname + "/../public/files/products.csv";
+var stream = fs.createReadStream(csvfile);
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+    res.render('index', { title: 'Import CSV using NodeJS' });
+
+}).get('/import', function(req, res, next) {
+
+    var  products  = []
+    var csvStream = csv()
+        .on("data", function(data){
+         
+         var item = new Product({
+              name: data[0] ,
+              price: data[1]   ,
+              category: data[2],
+              description: data[3],
+              manufacturer:data[4] 
+         });
+         
+          item.save(function(error){
+            console.log(item);
+              if(error){
+                   throw error;
+              }
+          }); 
+
+    }).on("end", function(_data){
+        
+    });
+  
+    stream.pipe(csvStream);
+    res.json({success : "Data imported successfully.", status : 200});
+   
+  }).get('/fetchdata', function(req, res, next) {
+    
+    Product.find({}, function(err, docs) {
+        if (!err){ 
+            res.json({success : "Data Fetched Successfully", status : 200, data: docs});
+        } else {
+            throw err;
+        }
+    });
+
+});
+module.exports = router;
